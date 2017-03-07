6f9d89b628483353cb097eab87c294db26b5c34f
root <root@atspot.org>
2017-03-07 10:26:00 +0300

    rename again

--- html/index.php
+++
@@ -1,6 +0,0 @@
-<?php require 'header.php'; ?>
-    <script src="./js/components/project.js"></script>
-    <script src="./js/index-app.js"></script>
-    <div id="container" class="container"> 
-  </div><!-- END OF CONTAINER-->
-<?php require 'footer.php'; ?>
